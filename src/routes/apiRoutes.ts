adc602ad3f98b9d700a1909182be895d0c6c95ee
The Mist Dev <themistdev@gmail.com>
2023-08-28 08:59:46 +0100

    update

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -8,7 +8,7 @@ const auth = new AuthController();
 
 //AUTH
 router.route('/auth/login').post(jwtChecker, auth.loginController);
-router.route('/auth/register').post( auth.registerController);
+router.route('/auth/register').post(auth.registerController);
 
 //USERDATA
 router.route('/usersData/userData/:uid/update').put(jwtChecker);
